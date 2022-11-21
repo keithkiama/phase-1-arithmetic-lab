cf8c84cb8cda17f761c38e04b49bfa7bde5896c5
keithkiama <keithkiama2003@gmail.com>
2022-11-22 00:58:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = 62*1;
+
+const random = Math.ceil(Math.random());
+
+const num3 = 20;
+const num4 = 8;
+const mod = num3%num4;
+
+const max = Math.max(0,12,4,20);
